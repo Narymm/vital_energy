afdc0b971ec97700985c0370e743dbf1cc54b178
Narymm <narymm3190@gmail.com>
2024-09-04 00:51:13 +0200

    3rd_commit

--- vital_energy/vien.py
+++ vital_energy/vien.py
@@ -118,7 +118,7 @@ def write_to_google_sheets(user_data):
     sheet = client.open("ChatBotBD").worksheet("Energy")
 
     # Подготовка данных для записи
-    row = [datetime.now().strftime("%Y-%m-%d %H:%M:%S"), user_data['username'], user_data['telegram_account']] + user_data['answers'] + [user_data.get('addit_data', '')]
+    row = [datetime.now().strftime("%Y-%m-%d %H:%M:%S"), user_data['username'], user_data['telegram_account'], user_data['id']] + user_data['answers'] + [user_data.get('addit_data', '')]
     sheet.append_row(row)
 
 # Функция для обработки команды /start
@@ -144,6 +144,7 @@ async def start_survey(update: Update, context: CallbackContext) -> int:
 async def ask_name(update: Update, context: CallbackContext) -> None:
     context.user_data['username'] = update.message.text
     context.user_data['telegram_account'] = update.message.from_user.username  # Сохраняем аккаунт Telegram
+    context.user_data['id'] = update.message.from_user.id  # Сохраняем id Telegram
     
     # Инициализация переменной answers
     context.user_data['answers'] = []
@@ -176,9 +177,15 @@ async def handle_question(update: Update, context: CallbackContext) -> None:
         return QUESTION1 + current_state
     
     elif current_state == len(questions) and query.data == "Отправить данные":
-        await query.message.reply_text(
-            'Пожалуйста, введите данные:'
-        )
+        if context.user_data['telegram_account'] == None:
+             await query.message.reply_text(
+                 'Пожалуйста, введите <b><i>Данные</i></b> и укажите <b><i>Контакт</i></b>, по которому можно с вами связаться (аккаунт ТГ/телефон/почта):',
+                 parse_mode='HTML'
+            )
+        else:
+            await query.message.reply_text(
+                'Пожалуйста, введите данные:'
+            )
         return GET_DATA
 
     else:
